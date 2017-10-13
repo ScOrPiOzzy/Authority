ca15302eae4aa41aeef227c1d5f38a97c3a09e8e
ScOrPiOzzy <zzyscorpio@gmail.com>
2017-10-13 14:34:22 +0800

    clean

---
+++ AuthoritySystem/authority-api/src/main/java/com/cas/authority/vo/AuthorityEntity.java
@@ -0,0 +1,107 @@
+package com.cas.authority.vo;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class AuthorityEntity implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8388545476374249262L;
+	// 注册码
+	private String regCode;
+	// （系统盘）硬盘序列号
+	private String hddSer;
+	// 处理器序列号
+	private String cpuSer;
+	// 产品代号
+	private String productID;
+	// 过期时间(格式：yyyy-MM-dd)
+	private String fromDate;
+	// 过期时间(格式：yyyy-MM-dd)
+	private String endDate;
+	// 数字签名
+	private String sign;
+
+	private int node;
+
+	public String getRegCode() {
+		return regCode;
+	}
+
+	public void setRegCode(String regCode) {
+		this.regCode = regCode;
+	}
+
+	public String getHddSer() {
+		return hddSer;
+	}
+
+	public void setHddSer(String hddSer) {
+		this.hddSer = hddSer;
+	}
+
+	public String getCpuSer() {
+		return cpuSer;
+	}
+
+	public void setCpuSer(String cpuSer) {
+		this.cpuSer = cpuSer;
+	}
+
+	public String getProductID() {
+		return productID;
+	}
+
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+
+	public String getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getNode() {
+		return node;
+	}
+
+	public void setNode(int node) {
+		this.node = node;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	@Override
+	public String toString() {
+		return "\r\n---开始授权证书信息---\r\n" + //
+				"注册码： " + regCode + " \r\n"//
+				+ "系统盘序列号： " + hddSer + " \r\n" //
+				+ "处理器序列号： " + cpuSer + " \r\n" //
+				+ "产品代码： " + productID + " \r\n" //
+				+ "起始日期： " + fromDate + " \r\n" //
+				+ "失效日期： " + endDate + " \r\n"//
+				+ "节点数： " + node + " \r\n"//
+				+ "---结束授权证书信息---";//
+	}
+
+}
