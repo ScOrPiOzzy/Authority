e9a19080e4d279d1b2790348d8fbb6d547c21713
张振宇 <sco_pra@126.com>
2017-10-24 13:50:43 +0800

    无用的类

--- AuthoritySystem/authority-provider/src/main/java/com/cas/authority/dao/SalerMapper.java
+++
@@ -1,8 +0,0 @@
-package com.cas.authority.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.cas.authority.model.Saler;
-
-@Mapper
-public interface SalerMapper extends IMapper<Saler> {}
--- AuthoritySystem/authority-provider/src/main/java/com/cas/authority/service/SalerService.java
+++
@@ -1,9 +0,0 @@
-package com.cas.authority.service;
-
-import com.cas.authority.model.Saler;
-
-/**
- * Created by CodeGenerator on 2017/10/13.
- */
-public interface SalerService extends Service<Saler> {
-}
